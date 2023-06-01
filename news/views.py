34e1b20d9d596e72be690300aed3518d151d5d89
Wiktor <wpiela9@gmail.com>
2023-06-01 08:08:21 +0200

    generic views added

--- news/views.py
+++ news/views.py
@@ -8,30 +8,11 @@ from rest_framework.views import APIView
 from django.http import Http404
 
 #all articles list and creating new one       
-class ArticleList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class ArticleList(generics.ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
 
 #get single article details, delete or update them
-class ArticleDetails(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class ArticleDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
- 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-
-
