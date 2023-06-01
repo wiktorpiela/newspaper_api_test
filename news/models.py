2409be2104ca52f628dda1de3042c15fb145b0e5
Wiktor <wpiela9@gmail.com>
2023-06-01 09:01:16 +0200

    token auth added

--- news/models.py
+++ news/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
@@ -9,4 +12,10 @@ class Article(models.Model):
 
     def __str__(self):
         return f"{self.title}, {self.author}"
+    
+@receiver(post_save, sender=User) #or sender=settings.AUTH_USER_MODE
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
 
--- newspaper_api/urls.py
+++ newspaper_api/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("get-token/", views.obtain_auth_token),
     path("", include("news.urls")),
 ]
