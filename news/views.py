fe3e828f83d00dafde75c48d3e108a4f350bdcfb
Wiktor <wpiela9@gmail.com>
2023-06-02 17:14:20 +0200

    permissions added

--- news/views.py
+++ news/views.py
@@ -7,18 +7,26 @@ from rest_framework import status, mixins, generics
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from django.http import Http404
+from rest_framework.permissions import AllowAny, IsAdminUser
+from .permission import IsOwnerReadOnly
 
 #all articles list and creating new one       
 class ArticleList(generics.ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
+
 #get single article details, delete or update them
 class ArticleDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    permission_classes = [IsOwnerReadOnly]
+
 
 #display all users and create new one
 class UserView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_permissions(self):
+        return [IsAdminUser() if self.request.method == "GET" else AllowAny()]
--- newspaper_api/settings.py
+++ newspaper_api/settings.py
@@ -38,6 +38,11 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES":[
         "rest_framework.authentication.TokenAuthentication",
     ],
+
+    "DEFAULT_PERMISSION_CLASSES":[
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+    ],
+
 }
 
 MIDDLEWARE = [
