a9a409399df3b4c2ec3a3e9fe9a7efbcc4b80327
Wiktor <wpiela9@gmail.com>
2023-06-20 19:27:12 +0200

    frontend replace inner html with random articles content

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,40 @@
 const btn = document.querySelector("button")
-const title = document.querySelector("h1")
+const title = document.querySelector("h2")
+const text = document.querySelector("p")
+const date = document.querySelector("small")
+const author = document.querySelector("h3")
 const myUrl = "http://127.0.0.1:8000/article-list/"
+let articles
+let article
 
-async function logJSONData(url) {
+const getRandomElement = list => {
+  return list[Math.floor(Math.random()*list.length)]
+}
+
+// function displayData(){
+//   title.innerHTML = article.title
+//   text.innerHTML= article.text
+//   date.innerHTML= article.date.slice(0,10)
+//   author.innerHTML= article.author
+// }
+
+async function getArticles(url) {
     const response = await fetch(url);
     const jsonData = await response.json();
-    console.log(jsonData);
+    return jsonData
   }
 
-logJSONData(myUrl)
+getArticles(myUrl)
+.then((data) => {
+  articles = data.results
+  btn.addEventListener("click", () => {
+    article = getRandomElement(articles)
+    title.innerHTML = article.title
+    text.innerHTML= article.text
+    date.innerHTML= article.date.slice(0,10)
+    author.innerHTML= article.author
+  })
+})
+
+
+
--- news/paginators.py
+++ news/paginators.py
@@ -1,4 +1,4 @@
 from rest_framework.pagination import PageNumberPagination
 
 class ArticlePaginator(PageNumberPagination):
-    page_size = 2
+    page_size = 5
